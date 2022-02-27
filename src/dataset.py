34fae8217f3f802c5a2cb0ccd37119e7d2c4cb5a
Ameya Parab <ameyaparab@novartis.com>
2022-02-27 11:04:56 -0500

    implemented data augmentation

--- src/dataset.py
+++ src/dataset.py
@@ -5,7 +5,6 @@ import numpy as np
 import pandas as pd
 import torch
 from PIL import Image
-from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset
 from torchvision import transforms
@@ -14,72 +13,113 @@ sys.path.insert(0, os.path.join(os.getcwd(), ".."))
 
 from config import BREED, CROPPED_DIM, DATA_DIR, IMAGE_DIM
 
-from src.utils import set_random_seed
+from src.utils import seed_worker, set_random_seed
+
+MEAN = [0.485, 0.456, 0.406]
+STD = [0.229, 0.224, 0.225]
+TRANSFORMS = {
+    "augmented": transforms.Compose(
+        [
+            transforms.RandomResizedCrop(size=IMAGE_DIM),
+            transforms.RandomRotation(degrees=30),
+            transforms.ColorJitter(),
+            transforms.RandomHorizontalFlip(),
+            transforms.CenterCrop(size=CROPPED_DIM),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=MEAN, std=STD),
+        ]
+    ),
+    "nonaugmented": transforms.Compose(
+        [
+            transforms.Resize(size=IMAGE_DIM),
+            transforms.CenterCrop(size=CROPPED_DIM),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=MEAN, std=STD),
+        ]
+    ),
+}
 
 
 class Dogs(Dataset):
-    def __init__(self, split: str):
+    def __init__(self, split: str, dataset: pd.DataFrame, augment: bool = False):
 
         self.split = split
-        self.image_files = os.listdir(os.path.join(DATA_DIR, split))
-        if split == "train":
-            label_encoder = preprocessing.LabelEncoder()
-            label_encoder.fit(BREED)
-            self.labels = torch.from_numpy(
-                label_encoder.transform(
-                    pd.read_csv(os.path.join(DATA_DIR, "labels.csv")).breed.values
-                )
-            ).type(torch.LongTensor)
-        else:
-            self.labels = torch.zeros(len(self.image_files)).type(torch.LongTensor)
+        self.dataset = dataset
+        self.transform_pipeline = TRANSFORMS.get(
+            "augmented" if augment else "nonaugmented"
+        )
 
     def __len__(self):
-        return len(self.image_files)
+        return len(self.dataset.index)
 
     def __getitem__(self, idx):
 
-        transform_pipeline = transforms.Compose(
-            [
-                transforms.Resize((IMAGE_DIM, IMAGE_DIM)),
-                transforms.CenterCrop(CROPPED_DIM),
-                transforms.ToTensor(),
-                transforms.ConvertImageDtype(torch.float32),
-                transforms.Normalize(
-                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-                ),
-            ]
+        image = self.transform_pipeline(
+            Image.open(
+                os.path.join(DATA_DIR, self.split, f"{self.dataset.id[idx]}.jpg")
+            )
         )
+        if self.split == "train":
+            label = torch.tensor(BREED.index(self.dataset.breed[idx]))
+        else:
+            label = torch.tensor(0)
 
-        return (
-            transform_pipeline(
-                Image.open(os.path.join(DATA_DIR, self.split, self.image_files[idx]))
-            ),
-            self.labels[idx],
-        )
+        return image, label
 
 
-def fetch_dataset(random_seed: int, batch_size: int):
+def fetch_dataset(random_seed: int, batch_size: int, num_workers: int = 4):
 
     set_random_seed(random_seed)
 
-    dev_dataset = Dogs("train")
-
+    train = pd.read_csv(os.path.join(DATA_DIR, "labels.csv"))
     train_idx, valid_idx = train_test_split(
-        np.arange(len(dev_dataset.labels)),
+        np.arange(len(train.index)),
         test_size=0.2,
         shuffle=True,
-        stratify=dev_dataset.labels,
+        stratify=train.breed.values,
         random_state=random_seed,
     )
 
-    train_sampler = torch.utils.data.SubsetRandomSampler(train_idx)
-    valid_sampler = torch.utils.data.SubsetRandomSampler(valid_idx)
-
-    train_loader = torch.utils.data.DataLoader(
-        dev_dataset, batch_size=batch_size, sampler=train_sampler
+    train_dataset = Dogs(
+        split="train",
+        dataset=train.iloc[train_idx].reset_index(drop=True),
+        augment=True,
     )
-    valid_loader = torch.utils.data.DataLoader(
-        dev_dataset, batch_size=batch_size, sampler=valid_sampler
+    validation_dataset = Dogs(
+        split="train", dataset=train.iloc[valid_idx].reset_index(drop=True)
     )
 
-    return (train_loader, valid_loader)
+    generator = torch.Generator()
+    generator.manual_seed(0)
+
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        worker_init_fn=seed_worker,
+        generator=generator,
+        shuffle=True
+    )
+    valid_dataloader = torch.utils.data.DataLoader(
+        validation_dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        worker_init_fn=seed_worker,
+        generator=generator,
+        shuffle=False
+    )
+
+    test = pd.DataFrame(os.listdir(os.path.join(DATA_DIR, "test")), columns=["id"])
+
+    test_dataset = Dogs(split="test", dataset=test)
+
+    test_dataloader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        worker_init_fn=seed_worker,
+        generator=generator,
+        shuffle=False
+    )
+
+    return (train_dataloader, valid_dataloader, test_dataloader)
